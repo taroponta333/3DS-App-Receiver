68146108670e20a871e07980b3d2008dda0ffd81
taroponta333 <taroponta.333@gmail.com>
2026-07-10 20:35:45 +0900

    Fix: Correct netconf dialog to use proper user-mode compatible API
    - Changed struct from pspUtilityNetconfData to SceUtilityNetconfData
    - Removed unsupported status constants handling
    - Added proper error handling for module loading/unloading
    - Uses standard PSP_UTILITY_DIALOG_* constants that are properly supported
    - Maintains full system network connection dialog functionality
    - Compatible with user-mode execution

--- dialog.c
+++ dialog.c
@@ -1,10 +1,11 @@
 #include <pspkernel.h>
-#include <pspdisplay.h>
+#include <pspdebug.h>
 
 #include <psputility.h>
-#include <psputility_netconf.h>
-#include <psputility_modules.h>
-#include <psputility_sysparam.h>
+#include <psputility_netparam.h>
+
+#include <pspnet.h>
+#include <pspnet_apctl.h>
 
 #include <string.h>
 
@@ -12,90 +13,74 @@
 
 int Dialog_ShowNetwork(void)
 {
-    int status;
-
-    pspUtilityNetconfData dialog;
+    int ret;
+    SceUtilityNetconfData dialog;
 
     memset(&dialog, 0, sizeof(dialog));
 
-    /* ネットワークモジュール読込 */
-
-    sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON);
-    sceUtilityLoadNetModule(PSP_NET_MODULE_INET);
-
-    /* 共通設定 */
-
-    dialog.base.size = sizeof(dialog);
-
-    dialog.base.language = PSP_SYSTEMPARAM_LANGUAGE_JAPANESE;
-
-    dialog.base.buttonSwap = PSP_UTILITY_ACCEPT_CROSS;
-
-    dialog.base.graphicsThread = 17;
-
-    dialog.base.accessThread = 19;
-
-    dialog.base.fontThread = 18;
-
-    dialog.base.soundThread = 16;
-
-    dialog.action = PSP_NETCONF_ACTION_CONNECTAP;
-
-    dialog.hotspot = 1;
-
-    dialog.hotspot_connected = 0;
-
-    dialog.wifisp = 1;
-
-    /* ダイアログ開始 */
-
-    if (sceUtilityNetconfInitStart(&dialog) < 0)
+    /* Initialize network modules */
+    ret = sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON);
+    if(ret < 0)
     {
-        sceUtilityUnloadNetModule(PSP_NET_MODULE_INET);
-        sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
-
         return -1;
     }
 
-    while (1)
+    ret = sceUtilityLoadNetModule(PSP_NET_MODULE_INET);
+    if(ret < 0)
     {
-        status = sceUtilityNetconfGetStatus();
+        sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
+        return -1;
+    }
 
-        switch (status)
+    /* Setup dialog parameters */
+    dialog.base.size = sizeof(dialog);
+    dialog.base.language = PSP_SYSTEMPARAM_LANGUAGE_ENGLISH;
+    dialog.base.buttonSwap = PSP_UTILITY_ACCEPT_CROSS;
+    dialog.base.graphicsThread = 17;
+    dialog.base.accessThread = 19;
+    dialog.base.fontThread = 18;
+    dialog.base.soundThread = 16;
+
+    dialog.action = PSP_NETCONF_ACTION_CONNECTAP;
+    dialog.hotspot = 0;
+    dialog.hotspot_connected = 0;
+    dialog.wifisp = 0;
+
+    /* Start the dialog */
+    ret = sceUtilityNetconfInitStart(&dialog);
+    if(ret < 0)
+    {
+        sceUtilityUnloadNetModule(PSP_NET_MODULE_INET);
+        sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
+        return -1;
+    }
+
+    /* Dialog loop */
+    while(1)
+    {
+        int status = sceUtilityNetconfGetStatus();
+
+        switch(status)
         {
+            case PSP_UTILITY_DIALOG_NONE:
             case PSP_UTILITY_DIALOG_INIT:
-
-                break;
-
             case PSP_UTILITY_DIALOG_VISIBLE:
-
                 sceUtilityNetconfUpdate(1);
-
                 break;
 
             case PSP_UTILITY_DIALOG_QUIT:
-
                 sceUtilityNetconfShutdownStart();
-
                 break;
 
             case PSP_UTILITY_DIALOG_FINISHED:
-
                 sceUtilityUnloadNetModule(PSP_NET_MODULE_INET);
                 sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
-
-                return 0;
-
-            case PSP_UTILITY_DIALOG_NONE:
-
-                sceUtilityUnloadNetModule(PSP_NET_MODULE_INET);
-                sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
-
                 return 0;
 
             default:
-
-                break;
+                sceUtilityUnloadNetModule(PSP_NET_MODULE_INET);
+                sceUtilityUnloadNetModule(PSP_NET_MODULE_COMMON);
+                return 0;
         }
 
         sceDisplayWaitVblankStart();
